aa65fd0db70013adb62239ac001bbfc4a21765ab
vittinzin <vitoredua7@gmail.com>
2025-09-09 13:23:58 -0300

    Organizacao geral

--- src/main/java/org/vitor/mockito/BilheteService.java
+++
@@ -1,9 +0,0 @@
-package org.vitor.mockito;
-
-import org.vitor.model.Passageiro;
-import org.vitor.model.Viagem;
-
-public interface BilheteService {
-
-    void comprarBilhete(Passageiro passageiro, Viagem viagem, Double precoBase);
-}
